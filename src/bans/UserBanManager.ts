87611c2080c8c08d3f478ecfdfd34a090bede29f
Matthew <github-gpg@service.matthe815.dev>
2024-05-24 16:56:50 -0400

    feat: Add unbans

--- src/bans/UserBanManager.ts
+++ src/bans/UserBanManager.ts
@@ -19,6 +19,13 @@ export default class UserBanManager {
         }
     }
 
+    public async unban(user_id: string, guild_id: string) {
+        const connection = await this.client.db.getConnection()
+        await connection.execute("DELETE FROM modmail_bans WHERE user_id = ? AND guild_id = ?", [user_id, guild_id])
+
+        this.bans.push(new UserBan(this, { user_id, guild_id, banned_by: "" }))
+    }
+
     public async ban(user_id: string, guild_id: string) {
         const connection = await this.client.db.getConnection()
         await connection.execute("INSERT INTO modmail_bans(user_id, guild_id, banned_by) VALUES (?, ?, ?)", [user_id, guild_id, ""])
