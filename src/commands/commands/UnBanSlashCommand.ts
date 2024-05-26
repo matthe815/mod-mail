11e9efb5d669c4271c6d27eb284fa7c7d5413ffc
Matthew <github-gpg@service.matthe815.dev>
2024-05-26 15:03:53 -0400

    feat: Add unban

---
+++ src/commands/commands/UnBanSlashCommand.ts
@@ -0,0 +1,28 @@
+import SlashCommandManager from "../SlashCommandManager";
+import SlashCommand from "../SlashCommand";
+import {CommandInteraction} from "discord.js";
+import {RelayDirection} from "../../mail/ModMail";
+
+export default class UnBanSlashCommand extends SlashCommand {
+    constructor(manager: SlashCommandManager) {
+        super(manager, {
+            name: "threadunban",
+            description: "Ban the originator of the thread from making new ones."
+        });
+
+        this.builder.setDefaultMemberPermissions(0x4)
+    }
+
+    execute(interaction: CommandInteraction) {
+        super.execute(interaction);
+        if (!interaction.channel || !interaction.channel.isThread()) return
+
+        const currentMail = this.manager.client.mail.getThreadMail(interaction.channel.id)
+        if (!currentMail) return
+
+        this.manager.client.bans.unban(currentMail.user_id, interaction.guildId || "")
+
+        currentMail.relay({ content: `You have been unbanned from sending Mod Mail messages in ${interaction.guild?.name}.` }, RelayDirection.User)
+        interaction.reply({ content: `The user has been unbanned from making mod mail.`, flags: 64 })
+    }
+}
